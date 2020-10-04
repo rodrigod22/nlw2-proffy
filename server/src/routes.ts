efc5fbe3a5d6380b56c3ed2f3362240bb830b62f
rodrigo <rodrigogat5@hotmail.com>
2020-10-04 15:40:38 -0300

    final 2 dia backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,69 +1,18 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinute from './utils/convetHourToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
 
 const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
+routes.post('/classes', classesController.create);
+routes.get('/classes', classesController.index);
 
-routes.post('/classes', async (request, response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
-  const trx = await db.transaction();
-
-  try {
-
-    const insertUsersIds = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    })
-
-    const user_id = insertUsersIds[0];
-
-    const insertedClassesIds = await trx('classes').insert({
-      subject,
-      cost,
-      user_id,
-    })
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinute(scheduleItem.from),
-        to: convertHourToMinute(scheduleItem.to),
-      };
-    })
-
-    await trx('class_schedule').insert(classSchedule);
-
-    await trx.commit();
-
-    return response.status(201).send();
-
-  } catch (err) {
-    await trx.rollback();
-    return response.status(400).json({ error: "Erro inexperado" + err })
-  }
-
-
-});
 
 
 export default routes;
